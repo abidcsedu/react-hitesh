45dcad28cca80e0b0227ec64fd2ea7af6ff93bad
Md. Abid Raihan <abidcsedu@gmail.com>
2024-02-18 18:52:28 +0600

    yet to debug

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,14 +1,18 @@
-import LogoutBtn from '../components/Header/LogoutBtn';
+import AuthLayout from "./AuthLayout";
+import Button from "./Button";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Login from "./Login";
 import Logo from "./Logo";
-import Container from './container/Container';
-
+import PostCard from "./PostCard";
+import RTE from "./RTE";
+import Select from "./Select";
+import Signup from "./Signup";
+import Container from "./container/Container";
+import PostForm from "./post-form/PostForm";
 export {
-    Container,
-    Footer,
-    Header,
-    Logo,
-    LogoutBtn
+    AuthLayout, Button, Container, Footer, Header, Input, Login, Logo,
+    LogoutBtn, PostCard, PostForm, RTE, Select, Signup
 };
-
